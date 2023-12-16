3e91d316428a81a1104144af969d4df71a7ba44e
Andriele Mendonça <dielemendonca@hotmail.com>
2023-12-16 17:39:24 -0300

    images added and README updated

--- hashtag-movies/README.md
+++ hashtag-movies/README.md
@@ -1,2 +1,57 @@
+<<<<<<< HEAD
+# Project "Hashtag Movies"
+=======
+
+>>>>>>> f8a7ddad362204209424f60cfac83edf35444580
+
+Project idea and database provided by Hashtag Programação.
+
+Hashtag Movie is a fictional movie rental company.
+
+The database is made up of 5 tables, as follow:
+
+![Columns of the tables](images/tables.png "Columns of the tables.")
+
+## Customer satisfaction with the catalog 
+
+Firstly, I assessed customer satisfaction with the service, based on the overall average rating of the films offered.
+
+We observed that the sample consists of 578 rentals, referring to 71 films, and that 43% of rented films were not rated.
+
+The average, disregarding null values, is 7.9, which can be considered average satisfaction. Among the available assessments, we observed that 58.8% had an above average score.
+
+What to do to increase customer satisfaction? Invest in the films that customers like most.
+
+To do this, we need to find out which genres have the best rating in the catalog.
+
+Looking at the average by genre, we see that 4 are above average: Action and Adventure, Art, Fiction and Fantasy and Animation. Among which,
+the two with the highest average are Action and Adventure and Art.
+
+![Average of rating per genre](images/media-por-genero.png "Average of rating per genre.")
+
+We can also go further and look for the genre with the highest number of excellent reviews (10 and 9).
+
+In absolute numbers, we observed a predominance of Drama films receiving excellent ratings, but, in percentage terms, this predominance does not prevail, as there is a predominance of the drama genre among the films rented.
+The predominance of excellent reviews belongs to the Action and Adventure genre, with 71.4% of reviews 9 and 10, followed by the Art genre, with 66.7%.
+
+I also looked at how ratings are distributed by year of film release.
+We observe that, grouping the averages by year, the years with films with ratings above the average are, in order:
+2009,2012,2001,2004,2007 and 2014.
+
+![Average of rating per release year](images/media-ano.png "Average of rating per release year")
+
+I also grouped the average by year of release and genre, for a more complete analysis.
+
+![Average of rating per release year AND genre](images/media-ano-genero.png "Average of rating per release year AND genre.")
+
+And, considering that the majority of rented films were drama films, although poorly rated, seeking to increase customer satisfaction, I investigated in which year the highest rated drama films were produced, in order to improve the quality of films of this genre, which is the most searched.
+
+![Release year of the best rated drama films](images/drama-ano.png "Release year of the best rated drama films")
+
+Therefore, we observe that Hashtag Movie should invest in Action and Adventure films (especially from 2009 and 2012) and Art.
+It is also interesting to invest in Fiction and Fantasy films from 2001, and Drama films from 2011.
+
+## Customer engagement and preference by gender and region.
 
 
+## Company revenue
--- hashtag-movies/src/preferences-moves.sql
+++ hashtag-movies/src/preferences-moves.sql
@@ -1,24 +1,4 @@
 /* 
-
-A hashtagmovie é um banco de dados que tem como principal objetivo armazenar as informações de
-aluguéis e de desempenho dos filmes alugados pelo serviço de streaming da empresa ao longo do período.
-
-Com esses dados será possível fazer análises dos dados para tomadas de decisão de curto e longo prazo,
-como:
-i) Decisão de investir em determinados filmes baseado na sua popularidade (nota)
-ii) Número total de locações definirá a RECEITA DA EMPRESA
-iii) A média de avaliação dos filmes definirá a SATISFAÇÃO DOS CLIENTES
-iv) O número de clientes ativos definirá o ENGAJAMENTO DOS CLIENTES
-
-Além disso, esses dados serão um prato cheio para descobrir coisas como:
-• As preferências dos clientes de acordo com região e sexo
-• Popularidade dos filmes por gênero e ano de lançamento
-• Filmes com nota acima da média
-• Dentre outros
-
-*/
-
-/* 
 Em quais generos de filmes investir baseado na popularidade? 
 Número de filmes com nota acima da média e distribuição das médias por ano de lançamento.
 Satisfação dos clientes baseada na média geral dos filmes.
@@ -26,16 +6,43 @@ Satisfação dos clientes baseada na média geral dos filmes.
 */
 USE hashtagmovie;
 
-/* Primeiramente, iremos identificar em qual genero de filmes a hashtag movie deve investir, baseando-se nas notas dadas pelos clientes
 
+/*
 Observamos que a amostra é constituida de 578 alugueis, referentes a 71 filmes*/
 SELECT COUNT(id_filme) FROM alugueis;
 SELECT COUNT(id_filme) FROM filmes;
 
 /* Aqui, observamos que há valores nulos na amostra, correspondentes a 250. Ou seja, cerca de 43% dos filmes alugados não foram avaliados.*/
-SELECT nota, COUNT(id_filme) FROM alugueis WHERE nota IS NULL;
+SELECT COUNT(*) FROM alugueis WHERE nota IS NULL;
 
-/* Eis a amostra sem valores nulos de nota, agrupadas por genero*/
+/* Primeiramente, analisaremos a satisfação dos clientes, baseados na média geral de avaliação dos filmes.
+A média é 7.9, o que pode ser considerado uma satisfação mediana. */
+SELECT AVG(nota) FROM alugueis WHERE nota IS NOT NULL;
+
+/* Dentre as avaliações disponíveis, observamos que 58,8% apresentam nota acima da média (7.9) */
+SELECT COUNT(*) 
+FROM alugueis 
+WHERE nota IS NOT NULL 
+AND 
+nota > (SELECT AVG(nota) FROM alugueis);
+
+
+/* O que fazer para aumentar a satisfação dos clientes? Investir nos filmes que os clientes mais gostam.
+Para isso, precisamos descobrir quais gêneros possuem melhor avaliação no catálogo. 
+
+Observando a média por genero, vemos que 4 estão acima da média: Ação e aventura, arte, ficção e fantasia e animação. Dentre os quais, 
+os dois com maior média são Ação e aventura e Arte*/
+SELECT f.genero, AVG(a.nota) 
+FROM alugueis AS a
+JOIN
+filmes AS f
+USING(id_filme)
+WHERE a.nota IS NOT NULL 
+GROUP BY f.genero
+ORDER BY AVG(a.nota) DESC;
+
+/* Podemos ainda, ir mais além e buscar os gênero com maior número de avaliações excelentes (10 e 9)
+ Eis a amostra sem valores nulos de nota, agrupadas por genero*/
 SELECT f.genero, COUNT(a.id_filme) 
 FROM alugueis AS a 
 JOIN
@@ -44,15 +51,11 @@ USING(id_filme)
 WHERE nota IS NOT NULL 
 GROUP BY f.genero;
 
-/* Dentre as avaliações disponíveis, observamos que 82,6% apresentam nota >= 7, sendo 7 a nota mais comum */
-SELECT nota, COUNT(id_filme) 
-FROM alugueis WHERE nota IS NOT NULL 
-GROUP BY nota ORDER BY nota DESC;
-
 /* Cerca de 52% das notas 10 foram dadas a filmes de drama.
 No entanto, essa quantidade corresponde a apenas 20,5% dos filmes de drama, considerando a quantidade total de filmes deste genero
 que foram avaliados.
 
+Avaliemos, primeiramente, apenas as notas 10.
 Quem recebeu, percentualmente, o maior número de notas 10 foi o genero Ação e Aventura
  */
 SELECT f.genero, COUNT(a.id_filme) 
@@ -73,12 +76,33 @@ USING(id_filme)
 WHERE a.nota=10 OR a.nota=9
 GROUP BY f.genero;
 
-/* Confiramando os resultados das queries anterioes, o gênero mais bem avaliado é Ação e aventura,
-seguido de Arte. */
-SELECT f.genero, AVG(a.nota)
-FROM filmes AS f
+/* 
+Podemos ainda, olhar como se distribuem as avaliação por ano de lançamento dos filmes.
+Observamos que, agrupando as médias por ano, os anos com filmes com avaliação acima da média são, em ordem:
+2009,2012,2001,2004,2007 e 2014. 
+*/
+
+SELECT f.ano_lancamento, AVG(a.nota) 
+FROM alugueis AS a
 JOIN
-alugueis AS a
+filmes AS f
 USING(id_filme)
-GROUP BY f.genero
+WHERE a.nota IS NOT NULL 
+GROUP BY f.ano_lancamento HAVING AVG(a.nota) > (SELECT AVG(nota) FROM alugueis WHERE nota IS NOT NULL)
 ORDER BY AVG(a.nota) DESC;
+
+/* Agrupando as médias por gênero e ano de lançamento */
+SELECT f.genero, f.ano_lancamento, AVG(a.nota), COUNT(*) 
+FROM alugueis AS a
+JOIN
+filmes AS f
+USING(id_filme)
+WHERE a.nota IS NOT NULL 
+GROUP BY f.genero, f.ano_lancamento HAVING AVG(a.nota) > (SELECT AVG(nota) FROM alugueis WHERE nota IS NOT NULL)
+ORDER BY AVG(a.nota) DESC;
+
+
+/* 
+Assim, observamos que a Hashtag Movie deve investir em filmes de Ação e aventura(especialmente de 2009 e 2012) e Arte.
+Também é interessante investir em filmes de Ficção e Fantasia de 2001.
+ */
